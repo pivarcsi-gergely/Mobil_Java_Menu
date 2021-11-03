ed7df65281ee1f77251cdfbd7bf2fb631996be7b
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-03 18:36:40 +0100

    MainActivity-be init, clicklistener-ek a ket gombra

--- app/src/main/java/hu/petrik/menu/MainActivity.java
+++ app/src/main/java/hu/petrik/menu/MainActivity.java
@@ -2,13 +2,41 @@ package hu.petrik.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonNev, buttonInformacio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        buttonNev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonInformacio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+            }
+        });
+    }
+
+    public void init(){
+        buttonNev = findViewById(R.id.buttonNev);
+        buttonInformacio = findViewById(R.id.buttonInformacio);
     }
 }
